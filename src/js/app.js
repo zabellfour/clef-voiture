af86fd365f6c2518060bc69382108d2c8a597b1f
zabellfour <zabell2814@gmail.com>
2017-06-04 01:25:41 +0300

    part of main page added

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,45 @@
 // This file will be compiled into app.js and will not be minified.
 // Feel free with using ES6 here.
 
-import DE from './modules/dots';
 
-( ($) => {
-  'use strict';
 
-  // When DOM is ready
-  $(() => {
-    DE.dotsEffect();
-  });
+(($) => {
+    'use strict';
+
+    // When DOM is ready
+    $(() => {
+
+        //navigation
+        var navOpener = $('.nav-opener'),
+            navHolder = $('.nav');
+        navOpener.on('click', function(e) {
+            navHolder.toggleClass("nav-opened");
+            navOpener.toggleClass("nav-opened");
+        });
+        $('.owl-carousel').owlCarousel({
+            items: 4,
+            loop: true,
+            nav: true,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1
+
+                },
+
+                480: {
+                    items: 2
+                },
+
+                768: {
+                    items: 3
+                },
+                1024: {
+                    items: 4
+                }
+            }
+        });
+    });
+
 
 })(jQuery);
